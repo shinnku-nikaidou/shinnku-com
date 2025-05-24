a6d711d957ed5f98b9d3c2467ab0c60582be6685
shinnku <nikaidou@shinnku.com>
2025-05-24 12:58:07 +0000

    del print

--- app/api/aiintro/route.ts
+++ app/api/aiintro/route.ts
@@ -4,11 +4,12 @@ import { wikisearchpicture } from '@/algorithm/wiki'
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url)
   const name = searchParams.get('name') as string
+  console.log(name)
   const bg = await wikisearchpicture(name)
   const intro = await fetch(`http://localhost:2998/intro?name=${name}`)
   if (intro.ok) {
     const text = await intro.text()
-    console.log(text)
+    // console.log(text)
     if (text.includes('No results found.')) {
       return NextResponse.json({ bg: bg || null })
     } else {

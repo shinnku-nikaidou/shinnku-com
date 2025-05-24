dbbef7e8c63f1c9d3914e5a4948e6c264f07fb44
shinnku <nikaidou@shinnku.com>
2025-05-25 00:15:44 +0800

    mix ai_search with traditional

--- algorithm/search.ts
+++ algorithm/search.ts
@@ -14,7 +14,7 @@ const options: IFuseOptions<SearchItem> = {
   includeScore: true,
   ignoreLocation: true,
   ignoreFieldNorm: true,
-  threshold: 0.82,
+  threshold: 0.78,
   keys: ['id'],
 }
 
@@ -39,6 +39,7 @@ export function aggregate_builder(...b: Array<BucketFiles>) {
 }
 
 export async function ai_search(q: string, n: number): Promise<SearchItem[]> {
+  const queryjp = cn2jp(q)
   const queryai = await fetch(
     `http://localhost:2998/findname?name=${encodeURIComponent(q)}`,
   )
@@ -50,9 +51,42 @@ export async function ai_search(q: string, n: number): Promise<SearchItem[]> {
     })
 
   console.log(`AI answer: ${queryai}`)
-  const results = runsearch(q + ' ' + queryai, search_index).slice(0, n)
+  const fuse = new Fuse(search_index, options)
+  const ai_res = fuse
+    .search(q + ' ' + queryai)
+    .map((result) => ({ item: result.item, score: result.score }))
+  const traditional_results = fuse
+    .search(q + ' ' + queryjp)
+    .map((result) => ({ item: result.item, score: result.score }))
 
+  const results: Array<{ item: SearchItem; score: number | undefined }> = []
+  for (const res of ai_res) {
+    if (res.score) {
+      results.push(res)
+    }
+  }
+  for (const res of traditional_results) {
+    if (res.score) {
+      const existing_result_index = results.findIndex(
+        (r) => r.item.id === res.item.id,
+      )
+      if (existing_result_index !== -1) {
+        if (results[existing_result_index].score && res.score) {
+          results[existing_result_index].score =
+            (results[existing_result_index].score ?? 0) / 2 +
+            (res.score ?? 0) / 2
+        }
+      } else {
+        results.push(res)
+      }
+    }
+  }
   return results
+    .sort((a, b) => {
+      return (a.score ?? 0) - (b.score ?? 0)
+    })
+    .slice(0, n)
+    .map((result) => result.item)
 }
 
 export function default_search(q: string, n: number): SearchItem[] {

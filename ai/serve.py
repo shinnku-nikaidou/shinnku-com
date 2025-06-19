8055c7ce45eeff4e9579efcf5b1d8da5426fe36b
shinnku <nikaidou@shinnku.com>
2025-06-19 21:24:33 +0800

    move database

--- ai/serve.py
+++ ai/serve.py
@@ -8,13 +8,13 @@ from langchain_chroma import Chroma
 emb = HuggingFaceEmbeddings(model_name="BAAI/bge-large-zh-v1.5")
 
 vectorstore_intro = Chroma(
-    persist_directory="../backend/database/beg_rag_chroma_generate",
+    persist_directory="./database/beg_rag_chroma_generate",
     embedding_function=emb,
 )
 retriever_intro = vectorstore_intro.as_retriever()
 
 vectorstore_findname = Chroma(
-    persist_directory="../backend/database/beg_rag_chroma", embedding_function=emb
+    persist_directory="./database/beg_rag_chroma", embedding_function=emb
 )
 
 retriever_findname = vectorstore_findname.as_retriever()

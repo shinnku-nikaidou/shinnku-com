415191ce46ad6f659d4da3cbeb20fa6ae11ca22b
shinnku <nikaidou@shinnku.com>
2025-06-20 20:41:07 +0800

    save

--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -4,6 +4,7 @@ import Search from '@/components/search'
 import { SearchAnswer } from '@/components/search/answer'
 import { ai_search } from '@/algorithm/search'
 import { SearchIntro } from '@/components/search/intro'
+import { Separator } from '@/components/ui/separator'
 
 export default async function SearchPage({
   searchParams,
@@ -19,11 +20,12 @@ export default async function SearchPage({
         <div className='max-w-[720px] px-4 md:px-10'>
           <Search initialSearchTerm={q} />
         </div>
-        <div className='grid grid-cols-1 gap-4 px-4 pt-10 md:grid-cols-2 md:px-10'>
-          <div className='md:pr-20'>
+        <div className='grid grid-cols-1 gap-4 px-1 pt-10 md:grid-cols-[2fr_1px_1fr] md:px-10'>
+          <div className='md:pr-10'>
             <SearchAnswer answer={answer} />
           </div>
-          <div>
+          <Separator className='hidden md:block' orientation='vertical' />
+          <div className='md:pl-2'>
             <SearchIntro name={q} />
           </div>
         </div>

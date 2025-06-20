957c8192cd0813ba65d90d1cb08c3869d211e212
shinnku <nikaidou@shinnku.com>
2025-06-21 01:08:31 +0800

    delete unused fuse

--- backend/src/fuse/tests.rs
+++ backend/src/fuse/tests.rs
@@ -1,26 +1,6 @@
 use crate::fuse::lib::Fuse;
 
 #[test]
-fn multibyte_chars() {
-    let pat = "f";
-    let s = &[
-        "®∮ℕ⊆ℕ₀⊂ℤℚ",
-        "😊🥺😉😍😘😚",
-        "⡍⠜⠇⠑⠹ ⠺⠁⠎",
-        "გთხოვთ",
-        "ıntəˈnæʃənəl",
-        "γνωρίζω ἀπὸ",
-        "コンニチハ",
-    ];
-
-    assert!(
-        Fuse::default()
-            .search_text_in_iterable(pat, s.iter())
-            .is_empty()
-    );
-}
-
-#[test]
 fn multibyte_chars_indices() {
     let needle = "f";
     let s = "®f∮";
@@ -31,23 +11,4 @@ fn multibyte_chars_indices() {
     let r = &x.ranges[0];
 
     assert_eq!(&s[r.start..r.end], needle);
-}
-
-#[test]
-fn full_match_higher() {
-    let s = &["Syrup", "Syrup2", "Live", "Live2", "Live3"];
-
-    let fuse = Fuse::default();
-
-    let result1 = fuse.search_text_in_iterable("Syrup", s.iter());
-
-    assert_eq!(result1.len(), 2);
-    assert_eq!(result1[0].index, 0);
-    assert_eq!(result1[1].index, 1);
-
-    let result2 = fuse.search_text_in_iterable("live", s.iter());
-
-    assert_eq!(result2.len(), 3);
-    assert_eq!(result2[0].index, 2);
-    assert_eq!(result2[1].score, result2[2].score);
 }

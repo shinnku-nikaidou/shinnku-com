55d68d377b3b42b3b5e14fdba6159d09ea0d4293
Shinnku <nikaidou@shinnku.com>
2025-06-20 17:48:00 +0800

    feat(frontend): add loading bar for search

---
+++ frontend/app/search/loading.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Loading() {
+  return (
+    <div className='w-full p-4'>
+      <div className='h-1 w-full animate-pulse rounded bg-primary' />
+    </div>
+  )
+}

603c7dde11fcf5c0b60d694415475b484eb79f07
shinnku <nikaidou@shinnku.com>
2025-06-21 18:20:38 +0800

    fix show bug

--- frontend/components/blog/SideTreeItem.tsx
+++ frontend/components/blog/SideTreeItem.tsx
@@ -32,7 +32,7 @@ export const TreeItem = ({ node, level }: TreeItemProps) => {
     <nav className='select-none'>
       <Link
         className={cn(
-          'w-full cursor-pointer justify-start gap-2 rounded-xl px-3 py-2',
+          'flex w-full items-center cursor-pointer rounded-xl py-2',
           level === 0 ? 'mt-0' : 'mt-1',
           'hover:bg-default/40',
         )}
@@ -40,24 +40,21 @@ export const TreeItem = ({ node, level }: TreeItemProps) => {
         style={{ paddingLeft: `${level * 12 + 12}px` }}
         onClick={handleClick}
       >
-        <div className='flex items-center gap-2'>
-          {node.type === 'directory' ? (
-            <>
-              <ChevronRight
-                className={`transition-transform duration-200 ${
-                  isOpen ? 'rotate-90' : ''
-                }`}
-                size={16}
-              />
-              <FolderOpen className='text-warning' size={16} />
-            </>
-          ) : (
-            <FileText className='ml-5 shrink-0 text-primary' size={16} />
-          )}
-          <span className='text-wrap text-left text-sm'>{node.label}</span>
-        </div>
+        {node.type === 'directory' ? (
+          <>
+            <ChevronRight
+              className={`transition-transform duration-200 ${
+                isOpen ? 'rotate-90' : ''
+              }`}
+              size={16}
+            />
+            <FolderOpen className='text-warning' size={16} />
+          </>
+        ) : (
+          <FileText className='ml-5 shrink-0 text-primary' size={16} />
+        )}
+        <span className='ml-2 text-wrap text-left text-sm'>{node.label}</span>
       </Link>
-
       {node.type === 'directory' && isOpen && (
         <div className='overflow-hidden'>
           {node.children?.map((child, index) => (

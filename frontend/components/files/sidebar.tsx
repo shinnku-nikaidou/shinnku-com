13ffb28bb8579ee8c334d5a2b12ded7b03497a8a
shinnku <nikaidou@shinnku.com>
2025-06-21 20:57:45 +0800

    save

--- frontend/components/files/sidebar.tsx
+++ frontend/components/files/sidebar.tsx
@@ -17,14 +17,14 @@ export const Sidebar = () => {
             asChild
             className='flex md:hidden'
             size='icon'
-            variant={pathname === item.link ? 'default' : 'outline-solid'}
+            variant={pathname === item.link ? 'default' : 'outline'}
           >
             <Link href={item.link}>{item.body}</Link>
           </Button>
           <Button
             asChild
             className='hidden justify-start md:flex w-full'
-            variant={pathname === item.link ? 'default' : 'outline-solid'}
+            variant={pathname === item.link ? 'default' : 'outline'}
           >
             <Link className='flex items-center gap-2' href={item.link}>
               {item.body}

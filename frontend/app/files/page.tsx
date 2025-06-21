16079bcc9167c50dd73f7c5a4d5d991db9b644c9
shinnku <nikaidou@shinnku.com>
2025-06-21 19:18:30 +0800

    save

--- frontend/app/files/page.tsx
+++ frontend/app/files/page.tsx
@@ -8,13 +8,13 @@ import Link from 'next/link'
 
 export default function FilesPage() {
   return (
-    <div className='container mx-auto flex flex-col items-center text-center'>
+    <div className='container mx-auto flex flex-col items-center text-center mt-10'>
       <h1 className={titleStyle()}>{t('allGames')}</h1>
       <div className='mt-8 grid grid-cols-2 gap-4 pt-10 sm:grid-cols-4'>
         {IndexList.map((item, index) => (
           <Link key={index} className='w-full' href={item.link}>
             <Card className='w-full shadow-sm'>
-              <CardContent className='flex flex-col size-36 items-center justify-center gap-2 overflow-visible'>
+              <CardContent className='flex flex-col size-36 items-center justify-center gap-2 overflow-visible mt-1'>
                 <span>{item.body}</span>
                 <b className='whitespace-nowrap overflow-hidden max-w-full'>
                   {item.title}

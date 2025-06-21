e1d8266abeff2a16ce2159beb131dccaf79cadfd
shinnku <nikaidou@shinnku.com>
2025-06-21 19:23:07 +0800

    save

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -3,9 +3,7 @@
 import type { ThemeProviderProps } from 'next-themes'
 
 import { AppProgressProvider as ProgressProvider } from '@bprogress/next'
-import { HeroUIProvider } from '@heroui/system'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { useRouter } from 'next/navigation'
 import * as React from 'react'
 
 export interface ProvidersProps {
@@ -14,20 +12,16 @@ export interface ProvidersProps {
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
-  const router = useRouter()
-
   return (
-    <HeroUIProvider navigate={router.push}>
-      <NextThemesProvider attribute='class' {...themeProps}>
-        <ProgressProvider
-          shallowRouting
-          color='#006FEE'
-          height='4px'
-          options={{ showSpinner: false }}
-        >
-          {children}
-        </ProgressProvider>
-      </NextThemesProvider>
-    </HeroUIProvider>
+    <NextThemesProvider attribute='class' {...themeProps}>
+      <ProgressProvider
+        shallowRouting
+        color='#006FEE'
+        height='4px'
+        options={{ showSpinner: false }}
+      >
+        {children}
+      </ProgressProvider>
+    </NextThemesProvider>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
